0ab6fd7d6d1383482890e67991cdf56cdd723690
Saranyoo Manitsan <saranyoo.manitsan@oivan.com>
2026-06-14 18:44:01 +0700

    remove automatic 10-second session checking interval, keeping only window focus and visibility change session checks

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,18 @@
 <script>
 import { getAuthToken, restoreSession } from '@/utils/auth';
 
-const SESSION_CHECK_INTERVAL_MS = 10000;
-
 export default {
   name: 'App',
-  data() {
-    return {
-      sessionCheckIntervalId: null,
-    };
-  },
   async created() {
     await this.checkSession();
   },
   mounted() {
     window.addEventListener('focus', this.handleWindowFocus);
     document.addEventListener('visibilitychange', this.handleVisibilityChange);
-    this.sessionCheckIntervalId = window.setInterval(this.checkSession, SESSION_CHECK_INTERVAL_MS);
   },
   beforeUnmount() {
     window.removeEventListener('focus', this.handleWindowFocus);
     document.removeEventListener('visibilitychange', this.handleVisibilityChange);
-
-    if (this.sessionCheckIntervalId) {
-      window.clearInterval(this.sessionCheckIntervalId);
-      this.sessionCheckIntervalId = null;
-    }
   },
   methods: {
     async checkSession() {
